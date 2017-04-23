45502ac87ee290508a4b945a4a93ad69251fab16
Dmitry <stormbringer-s@mail.ru>
2017-04-23 15:37:37 +0400

    added t4 class generator

---
+++ T4ClassGeneratorTry/T4ClassGeneratorTry/Mytt.cs
@@ -0,0 +1,274 @@
+public class vStoreWithContacts
+{
+}
+public class ScrapReason
+{
+}
+public class vStoreWithAddresses
+{
+}
+public class vVendorWithContacts
+{
+}
+public class Shift
+{
+}
+public class vVendorWithAddresses
+{
+}
+public class ProductCategory
+{
+}
+public class ShipMethod
+{
+}
+public class ProductCostHistory
+{
+}
+public class ProductDescription
+{
+}
+public class ShoppingCartItem
+{
+}
+public class ProductDocument
+{
+}
+public class DatabaseLog
+{
+}
+public class ProductInventory
+{
+}
+public class SpecialOffer
+{
+}
+public class ErrorLog
+{
+}
+public class ProductListPriceHistory
+{
+}
+public class Address
+{
+}
+public class SpecialOfferProduct
+{
+}
+public class ProductModel
+{
+}
+public class AddressType
+{
+}
+public class StateProvince
+{
+}
+public class ProductModelIllustration
+{
+}
+public class AWBuildVersion
+{
+}
+public class ProductModelProductDescriptionCulture
+{
+}
+public class BillOfMaterials
+{
+}
+public class Store
+{
+}
+public class ProductPhoto
+{
+}
+public class ProductProductPhoto
+{
+}
+public class TransactionHistory
+{
+}
+public class ProductReview
+{
+}
+public class BusinessEntity
+{
+}
+public class TransactionHistoryArchive
+{
+}
+public class ProductSubcategory
+{
+}
+public class BusinessEntityAddress
+{
+}
+public class ProductVendor
+{
+}
+public class BusinessEntityContact
+{
+}
+public class UnitMeasure
+{
+}
+public class Vendor
+{
+}
+public class ContactType
+{
+}
+public class CountryRegionCurrency
+{
+}
+public class CountryRegion
+{
+}
+public class WorkOrder
+{
+}
+public class PurchaseOrderDetail
+{
+}
+public class CreditCard
+{
+}
+public class Culture
+{
+}
+public class WorkOrderRouting
+{
+}
+public class Currency
+{
+}
+public class PurchaseOrderHeader
+{
+}
+public class CurrencyRate
+{
+}
+public class Customer
+{
+}
+public class Department
+{
+}
+public class Document
+{
+}
+public class SalesOrderDetail
+{
+}
+public class EmailAddress
+{
+}
+public class Employee
+{
+}
+public class SalesOrderHeader
+{
+}
+public class EmployeeDepartmentHistory
+{
+}
+public class EmployeePayHistory
+{
+}
+public class SalesOrderHeaderSalesReason
+{
+}
+public class SalesPerson
+{
+}
+public class Illustration
+{
+}
+public class JobCandidate
+{
+}
+public class Location
+{
+}
+public class Password
+{
+}
+public class SalesPersonQuotaHistory
+{
+}
+public class Person
+{
+}
+public class SalesReason
+{
+}
+public class SalesTaxRate
+{
+}
+public class PersonCreditCard
+{
+}
+public class vAdditionalContactInfo
+{
+}
+public class PersonPhone
+{
+}
+public class vEmployee
+{
+}
+public class SalesTerritory
+{
+}
+public class vEmployeeDepartment
+{
+}
+public class PhoneNumberType
+{
+}
+public class vEmployeeDepartmentHistory
+{
+}
+public class vIndividualCustomer
+{
+}
+public class Product
+{
+}
+public class vPersonDemographics
+{
+}
+public class vJobCandidate
+{
+}
+public class vJobCandidateEmployment
+{
+}
+public class vJobCandidateEducation
+{
+}
+public class vProductAndDescription
+{
+}
+public class vProductModelCatalogDescription
+{
+}
+public class vProductModelInstructions
+{
+}
+public class vSalesPerson
+{
+}
+public class SalesTerritoryHistory
+{
+}
+public class vSalesPersonSalesByFiscalYears
+{
+}
+public class vStateProvinceCountryRegion
+{
+}
+public class vStoreWithDemographics
+{
+}
+
